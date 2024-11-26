51ada9fd5e05fddb1eb9734989ab65680f33cc4a
Gidi Rabi <118205453+GidiRabi@users.noreply.github.com>
2024-11-26 19:46:54 +0200

    nice

--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-	public float speed = 2f; // Movement speed
+    public float speed = 2f; // Movement speed
 
-	void Update()
-	{
-		// Get input for horizontal and vertical movement
-		float moveX = Input.GetAxis("Horizontal");
-		float moveY = Input.GetAxis("Vertical");
+    void Update()
+    {
+        // Get input for horizontal and vertical movement
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
 
-		// Move the player
-		Vector3 movement = new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
-		transform.position += movement;
-	}
+        // Move the player
+        Vector3 movement = new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
+        transform.position += movement;
+    }
 }
