10cc0eba10a70352b4b2c2a3b5fb95931d24aa2d
Gidi Rabi <118205453+GidiRabi@users.noreply.github.com>
2024-11-26 19:32:12 +0200

    more updates

---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+	public GameObject carPrefab; // The car prefab to spawn
+	public Transform spawnPoint; // Fixed spawn location
+	public float minSpawnTime = 0.1f; // Minimum time between spawns
+	public float maxSpawnTime = 0.5f; // Maximum time between spawns
+
+	void Start()
+	{
+		StartCoroutine(SpawnCars());
+	}
+
+	IEnumerator SpawnCars()
+	{
+		while (true)
+		{
+			// Spawn the car at the fixed spawn point
+			Instantiate(carPrefab, spawnPoint.position, spawnPoint.rotation);
+
+			// Wait for a random time interval before the next spawn
+			float randomInterval = Random.Range(minSpawnTime, maxSpawnTime);
+			yield return new WaitForSeconds(randomInterval);
+		}
+	}
+}
---
+++ Assets/Scripts/DestroyCoinOnTouch.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCoinOnTouch : MonoBehaviour
+{
+	[Tooltip("The tag of the object that can destroy the coin")]
+	[SerializeField] private string triggeringTag = "Penguin"; // Default tag for the penguin
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		// Check if the object touching the coin has the triggering tag
+		if (other.CompareTag(triggeringTag))
+		{
+			Destroy(gameObject); // Destroy the coin
+		}
+	}
+}
